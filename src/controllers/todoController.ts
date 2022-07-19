f508fe7d0af0793937211525c45bb3d0165c0b39
Rodrigo Dalfré <67020557+rodrigodalfre@users.noreply.github.com>
2022-07-19 10:03:11 -0300

    projeto terminado

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -7,21 +7,46 @@ export const ping = async (req:Request, res:Response) => {
 }
 
 export const list = async (req:Request, res:Response) => {
-
-   res.json({})
+   const list = await Todo.findAll();
+   res.json({list})
 }
 
 export const create = async (req:Request, res:Response) => {
+   if(req.body.title) {
 
-   res.json({})
+      let newTodo = await Todo.create({
+         title: req.body.title,
+         done: req.body.done ? true : false
+      })
+      res.status(201).json({item: newTodo})
+      return
+   }
+
+   res.json({error: 'Dados não enviados'})
 }
 
 export const update = async (req:Request, res:Response) => {
+   const id = req.params.id as string
+   
+   let todo = await Todo.findByPk(id);
+   if(todo){
 
-   res.json({})
+      if(req.body.title) todo.title = req.body.title
+      todo.done = req.body.done ? true : false
+
+      await todo.save();
+      return res.json({todo})
+      
+   } else {
+      return res.json({error: 'Id não encontrado'})
+   }
 }
 
 export const remove = async (req:Request, res:Response) => {
-
-   res.json({remove: true})
+   const id = req.params.id as string
+   let todo = await Todo.findByPk(id);
+   if(todo) {
+      await todo.destroy();
+   }
+   return res.json({remove: true})
 }
